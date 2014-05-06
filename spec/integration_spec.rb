4b560fc9292772a7f08b8ee1ed9e40c00446db70
Jakub Racek <me@jakubracek.net>
2014-05-06 15:59:39 +0100

    No voucher specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,46 +3,61 @@ require 'user'
 describe "Integration" do
   let(:user) { User.new(voucher) }
 
-  describe 'default vouchers' do
-    let(:voucher) { Voucher.new(:default, credit: 15) }
+  describe 'no voucher' do
+    let(:voucher) { nil }
 
-    it 'should not bill user if has a remaining credit' do
-      user.bill
-      expect(user.orders[0].billed_for).to eql 0
-      user.bill
-      expect(user.orders[1].billed_for).to eql 0
-      user.bill
-      expect(user.orders[2].billed_for).to eql 1.1
+    it 'should bill default price all the time' do
+        user.bill
+        expect(user.orders[0].billed_for).to eql 6.95
+        user.bill
+        expect(user.orders[1].billed_for).to eql 6.95
+        user.bill
+        expect(user.orders[2].billed_for).to eql 6.95
     end
   end
 
-  describe 'discount vouchers' do
-    context 'pay as you go' do
-      let(:voucher) { Voucher.new(:discount, discount: 50, number: 3) }
+  describe 'vouchers' do
+    describe 'default vouchers' do
+      let(:voucher) { Voucher.new(:default, credit: 15) }
 
-      it 'should bill the right ammount' do
+      it 'should not bill user if has a remaining credit' do
         user.bill
-        expect(user.orders[0].billed_for).to eql 3.475
-        user.bill
-        expect(user.orders[1].billed_for).to eql 3.475
-        user.bill
-        expect(user.orders[2].billed_for).to eql 3.475
-        user.bill
-        expect(user.orders[3].billed_for).to eql 6.95
-      end
-    end
-
-    context 'pay with first order' do
-      let(:voucher) { Voucher.new(:discount, discount: 50, number: 3, instant: true) }
-      it 'should be paid instantly' do
-        user.bill
-        expect(user.orders[0].billed_for).to eql 10.425
+        expect(user.orders[0].billed_for).to eql 0
         user.bill
         expect(user.orders[1].billed_for).to eql 0
         user.bill
-        expect(user.orders[2].billed_for).to eql 0
-        user.bill
-        expect(user.orders[1].billed_for).to eql 6.95
+        expect(user.orders[2].billed_for).to eql 1.1
+      end
+    end
+
+    describe 'discount vouchers' do
+      context 'pay as you go' do
+        let(:voucher) { Voucher.new(:discount, discount: 50, number: 3) }
+
+        it 'should bill the right ammount' do
+          user.bill
+          expect(user.orders[0].billed_for).to eql 3.475
+          user.bill
+          expect(user.orders[1].billed_for).to eql 3.475
+          user.bill
+          expect(user.orders[2].billed_for).to eql 3.475
+          user.bill
+          expect(user.orders[3].billed_for).to eql 6.95
+        end
+      end
+
+      context 'pay with first order' do
+        let(:voucher) { Voucher.new(:discount, discount: 50, number: 3, instant: true) }
+        it 'should be paid instantly' do
+          user.bill
+          expect(user.orders[0].billed_for).to eql 10.425
+          user.bill
+          expect(user.orders[1].billed_for).to eql 0
+          user.bill
+          expect(user.orders[2].billed_for).to eql 0
+          user.bill
+          expect(user.orders[1].billed_for).to eql 6.95
+        end
       end
     end
   end
