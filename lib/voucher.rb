8e509fa760191fd3ad14976ad25f89719c8efaf7
tom <tommaso@pavese.me>
2016-01-12 23:33:29 +0000

    implement the spec'ed interface of Voucer

--- lib/voucher.rb
+++ lib/voucher.rb
@@ -1,7 +1,17 @@
 class Voucher
-  attr_accessor :credit, :type
+  attr_reader :type, :credit, :discount, :number, :instant
 
-  def self.create(type, *attrs)
+  def self.create(type=:default, **attrs)
+    new(type, attrs)
+  end
+
+
+  def initialize(type, **attrs)
+    @type     = type
+    @credit   = attrs[:credit]
+    @discount = attrs[:discount]
+    @number   = attrs[:number]
+    @instant  = attrs[:instant]
   end
 
   def billed_for
