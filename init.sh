a4d21fb5883622e4ba71e45919fd51bf38913655
tom <tommaso@pavese.me>
2016-01-12 23:10:20 +0000

    updated patch level for required MRI-2.0.0 (security fixes)

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ Path=$(dirname $0)
 cd $Path
 
 source /usr/local/rvm/scripts/rvm
-rvm use ruby-2.0.0-p195
+rvm use ruby-2.0.0-p648
 
 bundle install --gemfile $Path/Gemfile --path $Path/vendor/bundle
