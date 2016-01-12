8c89244f046b164acd52baad542f458c55cb926a
tom <tommaso@pavese.me>
2016-01-12 23:11:27 +0000

    project setup: smarter selection of rvm loading script in init.sh, added gitignore

--- init.sh
+++ init.sh
@@ -4,7 +4,18 @@ Path=$(dirname $0)
 
 cd $Path
 
-source /usr/local/rvm/scripts/rvm
+user_rvm_script="$HOME/.rvm/scripts/rvm"
+global_rvm_script="/usr/local/rvm/scripts/rvm"
+
+if [ -f $user_rvm_script ]; then
+    source $user_rvm_script
+elif [ -f $global_rvm_script ]; then
+    source $global_rvm_script
+else
+    echo "Please install RVM"
+    exit 1
+fi
+
 rvm use ruby-2.0.0-p648
 
 bundle install --gemfile $Path/Gemfile --path $Path/vendor/bundle
